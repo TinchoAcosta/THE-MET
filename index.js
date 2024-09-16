ed96a9f66c8ad9e91391c4bc40282ac0edb27d87
TinchoAcosta <soymartinacosta@hotmail.es>
2024-09-16 14:55:20 -0300

    duko

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const port = process.env.PORT || 3000
 
 app.use(morgan("dev"))
 
-app.listen(port, () => {console.log(`app funcionando en http://localhost:${port}`)})
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use("/", rutas)
 app.use('/api', (err, req, res, next) => {
-    if (err.status === 404) {
-      res.status(404).json({ error: 'Recurso no encontrado' });
-    } else {
-      res.status(500).json({ error: 'Error interno del servidor' });
-    }
-  });
+  if (err.status === 404) {
+    res.status(404).json({ error: 'Recurso no encontrado' });
+  } else {
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+app.listen(port, () => {console.log(`app funcionando en http://localhost:${port}`)})
